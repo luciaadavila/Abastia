b6a33d42e632c3c219d3d29bdd5c23e759299d54
Lucía Dávila <lucia@compostela.me>
2026-07-21 11:40:47 +0200

    enhancement/21: sidenav y variables globales

---
+++ frontend/src/app/components/lista-compra/lista-compra.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-compra',
+  imports: [],
+  templateUrl: './lista-compra.html',
+  styleUrl: './lista-compra.scss',
+})
+export class ListaCompra {}
---
+++ frontend/src/app/components/sidenav/sidenav.ts
@@ -0,0 +1,54 @@
+// Copyright (c) 2010-2026 Google LLC. https://angular.dev/license
+
+import { MediaMatcher } from '@angular/cdk/layout';
+import { Component, OnDestroy, inject, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
+
+@Component({
+  selector: 'sidenav',
+  templateUrl: 'sidenav.html',
+  styleUrl: 'sidenav.scss',
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet,
+  ],
+})
+export class Sidenav implements OnDestroy {
+  protected readonly isMobile = signal(true);
+
+  private readonly _mobileQuery: MediaQueryList;
+  private readonly _mobileQueryListener: () => void;
+
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
+
+  constructor() {
+    const media = inject(MediaMatcher);
+
+    this._mobileQuery = media.matchMedia('(max-width: 600px)');
+    this.isMobile.set(this._mobileQuery.matches);
+    this._mobileQueryListener = () => this.isMobile.set(this._mobileQuery.matches);
+    this._mobileQuery.addEventListener('change', this._mobileQueryListener);
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+
+  ngOnDestroy(): void {
+    this._mobileQuery.removeEventListener('change', this._mobileQueryListener);
+  }
+}
